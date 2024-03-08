f1b586dae9e3d706e29a5897aff382076731f9ea
Amit Kumar <113462014+amitkr-15@users.noreply.github.com>
2024-03-08 07:31:12 +0000

    Added user , video models

---
+++ src/models/video.model.js
@@ -0,0 +1,48 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+// we can use aggregation concept to uuse as plugin
+
+const videoSchema = new Schema(
+  {
+    videoFile :{
+      type : String , // cloudinary url
+      required : true
+    },
+    thumbnail : {
+      type : String , // cloudinary url
+      required : true
+    },
+    title : {
+      type : String,
+      required : true 
+    },
+    decription :{
+      type : String,
+      required : true
+    },
+    duratin :{
+      type : Number,
+      required :true 
+    },
+    views :{
+      type : Number,
+      default : 0
+    },
+    isPublished :{
+      type: Boolean,
+      default : true 
+    },
+    owner : {
+      type : Schema.Types.ObjectId,
+      ref  : "User"
+    }
+  },
+  {
+    timestamps: true 
+  }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video" , videoSchema)

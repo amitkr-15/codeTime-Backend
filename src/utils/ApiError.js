d0d37ef72790a6e4c4694cdbb32dc55c684ebd55
Amit Kumar <113462014+amitkr-15@users.noreply.github.com>
2024-08-22 22:10:10 +0530

    fix some bugs in the project

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -21,21 +21,12 @@ class ApiError extends Error {
 
     if(stack){
       this.stack = stack
-
-    if(stack){
-      this.stack = stack
-
-
-    if(stack){
-      this.stack = stack
-
     }else{
       Error.captureStackTrace(this , this.constructor)
     }
     
   }
 }
-}
-}
+
 
 export {ApiError}

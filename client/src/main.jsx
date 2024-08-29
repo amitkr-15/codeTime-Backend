759fdf5fb31ded3ae498af811f79adf6e033c56a
Amit Kumar <113462014+amitkr-15@users.noreply.github.com>
2024-08-29 18:41:10 +0530

    fix some bugs and error in client folder

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { ChakraProvider } from '@chakra-ui/react'
+import { Provider } from 'react-redux'
+import { store } from './Store/Store.js'
+import { BrowserRouter } from 'react-router-dom'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ChakraProvider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </ChakraProvider>,
+);

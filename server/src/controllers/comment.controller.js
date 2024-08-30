fa581ed33ea6bfa3a1e15f6f20276984bee92a21
Amit Kumar <113462014+amitkr-15@users.noreply.github.com>
2024-08-30 23:18:21 +0530

    import models in comment controller and fix some bug

--- server/src/controllers/comment.controller.js
+++ server/src/controllers/comment.controller.js
@@ -3,6 +3,9 @@ import {Comment} from "../models/comment.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { Video } from "../models/video.model.js"
+import { Tweet } from "../models/tweets.model.js"
+import verifypostowner from "../utils/checkforpostowner.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
